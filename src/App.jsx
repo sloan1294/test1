e8cbe4a65c1693abaad2d0338ce562b3160660c2
Ashton Sloan <sloan1294@hotmail.com>
2023-07-12 15:37:47 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  console.log(name, message);
+  return (
+    <div className="container">
+      <form name="contact" method="POST">
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="input">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+
+        <button type="submit">Get in touch!</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
